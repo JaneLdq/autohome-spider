066cbf7429621f7e9da41b3c6d0d5dd82a708564
Jane Liao <jane.liao@sap.com>
2018-02-03 17:52:46 +0800

    scraping autohome

--- autohomeSpider/settings.py
+++ autohomeSpider/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -65,7 +65,7 @@ COOKIES_ENABLED = False
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'autohomeSpider.pipelines.JsonWriterPipeline': 300,
+   'autohomeSpider.pipelines.MongoPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -88,3 +88,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI='localhost'
+MONGO_DATABASE='autohome'
--- autohomeSpider/spiders/articles_spider.py
+++ autohomeSpider/spiders/articles_spider.py
@@ -2,18 +2,19 @@
 
 from autohomeSpider.items import Article
 from requests.exceptions import RequestException
-from urllib.parse import urlencode
+from urllib.request import urlopen
+from urllib.parse import quote
+import json
 import scrapy
-import requests
 import re
 
 class ArticlesSpider(scrapy.Spider):
     name = "articles"
 
-    def start_requests(self, keyword='tesla', *args, **kwargs):
+    def start_requests(self, keyword='特斯拉', *args, **kwargs):
         super(ArticlesSpider, self).__init__(*args, **kwargs)
-        params = urlencode({'q': keyword})
-        url = 'https://sou.autohome.com.cn/wenzhang?%s' % params
+        keyword = quote(keyword.encode('gb2312'))
+        url = 'https://sou.autohome.com.cn/wenzhang?q=%s' % keyword
         yield scrapy.Request(url=url, callback=self.parse_search_page)
 
     def parse_search_page(self, response):
@@ -23,46 +24,24 @@ class ArticlesSpider(scrapy.Spider):
         # go to next page
         next_page = response.xpath("//a[@class='page-item-next']/@href").extract_first()
         if next_page is not None:
-            self.logger.info(next_page)
-            yield scrapy.Request(url=next_page, callback=self.parse_search_page)
+            yield response.follow(url=next_page, callback=self.parse_search_page)
 
     def parse_article_page(self, response):
         article = Article()
-        # id = re.compile(r'/(\d+).html$', response.url).findall()[0]
-        # self.logger.info(str(id))
-        # self.logger.info("")
-        article['title'] = response.xpath("//div[@id='articlewrap']/h1/text()").extract_first()
-        article['date'] = response.xpath("//div[@class='article-info']/span/text()")[0].extract()
+        article['id'] = re.compile(r'.*/(\d+).html$').findall(response.url)[0]
+        article['title'] = response.xpath("//div[@id='articlewrap']/h1/text()").extract_first().strip()
+        article['date'] = response.xpath("//div[@class='article-info']/span/text()")[0].extract().strip()
+        # TODO extract all plain text
         article['content'] = response.xpath("//div[@id='articleContent']/p[not(contains(@class, 'center'))]/text()").extract()
-
         # get all comments
         comments = []
-        # try:
-        #     comment_api = "https://reply.autohome.com.cn/api/comments/show.json?id=%s&appid=1&count=0" % id
-        #     request = requests.get(comment_api)
-        #     self.logger.info(request.json())
-        #     comment_list = request.json()['commentlist']
-        #     for c in comment_list:
-        #         comments.append(c['RContent'])
-        # except RequestException:
-        #     self.logger.info("Error occurs when get comments from: ", comment_api)
-        # finally:
-        article['comments'] = comments
-        yield article
-
-        # comment_page = response.xpath("//a[@id='hudongreply']/@href").extract()
-        # get comments of this article
-        # request = scrapy.Request(url=comment_page, callback=parse_comment_page)
-        # request.meta['article'] = article
-        # yield request
-
-
-    # def parse_comment_page(self, response):
-    #     article = reponse.meta['article']
-    #     # add comments in current_page to article
-    #     comments = response.xpath("//dl[@id='reply-list']/dd/@datacontent").extract()
-    #     article['comments'].append(comments)
-    #     # if next page exists, send request
-    #     next_page = response.xpath("//a[@class='page-item-next']/@gopages").extract()
-    #     if next_page is not None:
-    #         request = request
+        try:
+            comment_api = 'https://reply.autohome.com.cn/api/comments/show.json?id=%s&appid=1&count=0' % article['id']
+            request = json.load(urlopen(comment_api))
+            for c in request['commentlist']:
+                comments.append(c['RContent'])
+        except (URLError, HTTPError, JSONDecodeError):
+            self.logger.info("Error occurs when get comments from: ", comment_api)
+        finally:
+            article['comments'] = comments
+            yield article
